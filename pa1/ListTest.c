4af535237c7c317591d9e1deb781d8cab3e25863
Evin Odisho <edodisho@ucsc.edu>
2022-03-31 09:36:03 -0700

    Making test file with adaptation from ListClient

---
+++ pa1/ListTest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "List.h"
+
+int main(int argc, char *argv[]) {
+    List A = newList();
+    List B = newList();
+
+
+
+    return (EXIT_SUCCESS);
+}
