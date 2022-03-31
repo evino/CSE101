37648934974b6d37aa3ebd27278382743b09a831
Evin Odisho <edodisho@ucsc.edu>
2022-03-30 22:01:57 -0700

    All accessor functions implemented

--- pa1/List.c
+++ pa1/List.c
@@ -26,8 +26,142 @@ typedef struct ListObj {
     Node front;
     Node back;
     Node cursor;
+    int length;
 } ListObj;
 
 // Constructors-Destructors ---------------------
 
+// Returns reference to new node object. Initializes prev/next and data fields.
+Node newNode (int data) {
+    Node N = malloc(sizeof(NodeObj));
+    N->data = data;
+    N->previous = NULL;
+    N->next = NULL;
+    return (N);
+}
 
+// Frees heap mem pointed to by *pN, sets *pN to NULL
+void freeNode (Node *pN) {
+    if (pN != NULL *pN != NULL) {
+        free(*pN);
+        *pN = NULL;
+    }
+}
+
+List newList(void) {
+    List L;
+    L = malloc(sizeof(ListObj));
+    L->front = NULL;
+    L->back = NULL;
+    L->cursor = NULL;
+    L->index = -1;
+    L->length = 0;
+    return(L);
+}
+
+void freeList(List *pL) {
+    if(pL != NULL && *pL != NULL) {
+        while (!isEmpty(*pL)) {
+            delete(*pL);
+        }
+        free(*pL);
+        *pL = NULL;
+    }
+}
+
+
+// Access functions ------------------------------
+
+int length(List L) {
+    if (L == NULL) {
+        printf("List Error: calling length() on NULL List reference\n");
+        exit(EXIT_FAILURE);
+    }
+    return (L->length);
+}
+
+int index(List L) {
+    if (L == NULL) {
+        printf("List Error: calling index() on NULL List reference\n");
+        exit(EXIT_FAILURE);
+    }
+    if (isEmpty(L)) {
+        printf("List Error: calling index() on empty List\n");
+        exit(EXIT_FAILURE);
+    }
+    return (L->index);
+}
+
+int front(List L) {
+    if (L == NULL) {
+        printf("List Error: calling front() on NULL List reference\n");
+        exit(EXIT_FAILURE);
+    }
+    if (length(L) < 0) {
+        printf("List Error: calling front() on empty List\n");
+        exit(EXIT_FAILURE);
+    }
+    return (L->front->data);
+}
+
+int back(List L) {
+    if (L == NULL) {
+        printf("List Error: calling back() on NULL List reference\n");
+        exit(EXIT_FAILURE);
+    }
+    if (length(L) < 0) {
+        printf("List Error: calling back() on empty List\n");
+        exit(EXIT_FAILURE);
+    }
+    return(L->back->data);
+}
+
+
+int get(List L) {
+    if (L == NULL) {
+        printf("List Error: calling get() on NULL List reference\n");
+        exit(EXIT_FAILURE);
+    }
+    if (length(L) < 0) {
+        printf("List Error: calling get() on empty List\n");
+        exit(EXIT_FAILURE);
+    }
+    if (index(L) <= 0) {
+        printf("List Error: calling get() with undefined cursor\n");
+        exit(EXIT_FAILURE);
+    }
+    return (L->cursor->data);
+}
+
+bool equals(List A, List B) {
+    bool eq = false;
+    Node N = NULL;
+    Node M = NULL;
+
+    if (A == NULL || B == NULL) {
+        printf("List Error: calling equals() on NULL List reference\n");
+        exit(EXIT_FAILURE);
+    }
+
+    eq = (A->length == B->length);
+    N = A->front;
+    M = B->front;
+    while (eq && N!= NULL) {
+        eq = (N->data == M->data);
+        N =  N->next;
+        M = M->next;
+    }
+    return eq;
+}
+
+
+
+
+bool isEmpty(List L) {
+    if (L == NULL) {
+        printf("List Error: calling isEmpty() on NULL List reference\n");
+        exit(EXIT_FAILURE);
+    }
+       
+    return (L->length == 0);
+}
