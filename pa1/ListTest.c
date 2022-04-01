38f7561e2f42cd7a9bb338a2c6af6e2ae468a83b
Evin Odisho <edodisho@ucsc.edu>
2022-03-31 23:33:02 -0700

    added more functions to test

--- pa1/ListTest.c
+++ pa1/ListTest.c
@@ -5,9 +5,15 @@
 
 int main(int argc, char *argv[]) {
     List A = newList();
-    List B = newList();
+    //List B = newList();
+    //List C = NULL;
 
-
+    int i;
+    for(i=1; i<=20; i++) {
+        append(A, i);
+    }
+   
+    printList(stdout, A);
 
     return (EXIT_SUCCESS);
 }
