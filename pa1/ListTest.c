6565aec04a90e40fa33c022751b66a7895c70a95
Evin Odisho <edodisho@ucsc.edu>
2022-04-02 11:18:34 -0700

    Tests index, moves cursor and append/prints now

--- pa1/ListTest.c
+++ pa1/ListTest.c
@@ -7,12 +7,20 @@ int main(int argc, char *argv[]) {
     List A = newList();
     //List B = newList();
     //List C = NULL;
-
-    int i;
-    for(i=1; i<=20; i++) {
+    for(int i=1; i<=20; i++) {
+        printf("Appending: %d\n", i);
         append(A, i);
     }
-   
+    moveFront(A);
+    int myIndex = index(A);
+    printf("Index @: %d\n", myIndex);
+    moveNext(A);
+    printf("Index now at: %d\n", index(A));
+    moveNext(A);
+    printf("Index now at: %d\n", index(A));
+    moveBack(A);
+    myIndex = index(A);
+    printf("Index @ %d\n", myIndex);
     printList(stdout, A);
 
     return (EXIT_SUCCESS);
