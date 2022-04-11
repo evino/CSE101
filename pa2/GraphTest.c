adac9364d39590da162d1064206a13020bcc5f79
Evin Odisho <edodisho@ucsc.edu>
2022-04-11 16:46:48 -0700

    Added tests

--- pa2/GraphTest.c
+++ pa2/GraphTest.c
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[]) {
     int n = 5;
     Graph G = newGraph(n);
+    printf("Order is %d\n", getOrder(G));
     freeGraph(&G);
     printf("Success\n");
     return 0;
