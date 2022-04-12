7bc4e48d0b21a1d35df5edf4c0df73c8fccbaa79
Evin Odisho <edodisho@ucsc.edu>
2022-04-12 13:05:40 -0700

    working on addArch

--- pa2/Graph.c
+++ pa2/Graph.c
@@ -16,34 +16,25 @@ typedef struct GraphObj {
     int order;
     int size;
     int source;
-    int *vertLabel;
 } GraphObj;
 
 Graph newGraph(int n) {
     Graph G;
-    List L = newList();
     G = malloc(sizeof(GraphObj));
     G->listArr = malloc((n + 1) * sizeof(List));
     G->colors = malloc((n + 1) * sizeof(char));
     G->parArr = malloc((n + 1) * sizeof(int));
     G->distArr = malloc((n + 1) * sizeof(int));
-    G->vertLabel = malloc((n + 1) * sizeof(int));
 
-    G->listArr = NULL;
-    G->colors = NULL;
     G->order = n;
     G->size = 0;
     G->source = NIL;
     for (int i = 1; i < n + 1; i++) {
+        G->listArr[i] = newList();
         G->parArr[i] = NIL;
-    }
-
-    for (int i = 1; i < n + 1; i++) {
+        printf("DB1\n");
         G->distArr[i] = INF;
-    }
-    
-    for (int i = 1; i < n + 1; i++) {
-        G->vertLabel[i] = NIL;
+        printf("DB2\n");
     }
     //*(G->parArr) = NIL;
     //*(G->distArr) = INF;
@@ -64,7 +55,6 @@ void freeGraph(Graph *pG) {
     int n = getOrder(*pG);
     free((*pG)->parArr);
     free((*pG)->distArr);
-    free((*pG)->vertLabel);
     for (int i = 1; i < n + 1; i++) {
         pL = (*pG)->listArr[i];
         freeList(&pL);
@@ -107,4 +97,34 @@ int getSource(Graph G) {
 }
 */
 
+
+
+/*** Manipulation procedures ***/
+
+void makeNull(Graph G) {
+    if (G == NULL) {
+        printf("Graph Error: Calling makeNull() on NULL Graph reference\n");
+    }
+    G->size = 0;
+    return;
+}
+
+void addEdge(Graph G, int u, int v) {
+}
+
+void addArc(Graph G, int u, int v) {
+    if (u < 1 || u > getOrder(G)) {
+        printf("Graph Error: Calling addArc with out of range u arguement.\n");
+        exit(EXIT_FAILURE);
+    }
+    if (v < 1 || v > getOrder(G)) {
+        printf("Graph Error: Calling addArc with out of range v arguement.\n");
+        exit(EXIT_FAILURE);
+    }
+    // Do insertion sort here
+    if (G->listArr[1] == NULL) {
+        append(G->listArr[1], v);
+    }
+    return;
+}
 
