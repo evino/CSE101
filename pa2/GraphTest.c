5c4c792b8cff6464654fb06fd8578b0450aa71fa
Evin Odisho <edodisho@ucsc.edu>
2022-04-13 22:55:12 -0700

    Adding small tests

--- pa2/GraphTest.c
+++ pa2/GraphTest.c
@@ -3,9 +3,13 @@
 
 int main(int argc, char *argv[]) {
     int n = 5;
+    int u = 3;
+    int v = 2;
     printf("Making new graph\n");
     Graph G = newGraph(n);
     printf("Order is %d\n", getOrder(G));
+    addEdge(G, u, v);
+    printGraph(stdout, G);
     freeGraph(&G);
     printf("Success\n");
     return 0;
