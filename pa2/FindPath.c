b6dc05d4c4082f8a0fd0c12cefc78ce812a58904
Evin Odisho <edodisho@ucsc.edu>
2022-04-15 23:55:16 -0700

    trying to get path working

--- pa2/FindPath.c
+++ pa2/FindPath.c
@@ -13,10 +13,11 @@
 
 int main(int argc, char *argv[]) {
     
-    FILE *in, *out, *in2;
+    FILE *in, *out;
     int n;
     int u, v;
     Graph G = NULL;
+    List L = newList();
 
     if( argc != 3 ) {
         fprintf(stderr, "Usage: %s <input file> <output file>\n", argv[0]);
@@ -28,7 +29,6 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Unable to open file %s for reading\n", argv[1]);
     exit(EXIT_FAILURE);
     }
-    in2 = fopen(argv[1], "r");
     
     out = fopen(argv[2], "w");
     if( out==NULL ){
@@ -64,19 +64,19 @@ int main(int argc, char *argv[]) {
     int j = 0;
     int source = 1;
     int dest = 1;
-    int vertNum = 0;
     while ((source != 0 && dest != 0)) {
         if (j > 0) {
             fscanf(in, "%d %d", &source, &dest);
-            printf("U is %d and v is %d\n", source, dest);
             if (source == 0 && dest == 0) {
                 break;
             }
             BFS(G, source);
-            source = getSource(G);
-            fprintf(stdout, "The distance from %d to %d is %d\n", dest, source, getDist(G, dest));
+            //source = getSource(G);
+            fprintf(stdout, "The distance from %d to %d is %d\n", source, dest, getDist(G, dest));
+            fprintf(stdout, "A shortest %d-%d path is: ", source, dest); getPath(L, G, dest);
+            printList(stdout, L);
+            //fprintf(stdout, "A shortest %d-%d path is: "; getPath(L, G, dest));
         }
-        printf("Inc J here\n");
         j++;
     }
 
