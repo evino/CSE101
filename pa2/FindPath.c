02fea75934fa028294d3b572abc8ff4fff4e8419
Evin Odisho <edodisho@ucsc.edu>
2022-04-15 19:58:35 -0700

    Got io working

--- pa2/FindPath.c
+++ pa2/FindPath.c
@@ -7,32 +7,16 @@
 
 
 #include "Graph.h"
-#define MAX_LEN 10
-#define BUFF 2
-/*
-void extract(char *line[]) {
-    //int lineInt = atoi(*line);
-    char x1[2];
-    x1[0] = *line[0];
-    if (*line[1] != " ") {
-        x1[1] = *line[1];
-    }
-    printf("%d\n", atoi(x1));
 
-    return;
-}
-*/
+
 
 
 int main(int argc, char *argv[]) {
     
     FILE *in, *out;
-    int n = 0;
-    Graph G = NULL;
+    int n;
     int u, v;
-    char line[MAX_LEN];
-    char x1[2];
-    char y1[2];
+    Graph G = NULL;
 
     if( argc != 3 ) {
         fprintf(stderr, "Usage: %s <input file> <output file>\n", argv[0]);
@@ -51,51 +35,26 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    fscanf(in, "%d", &n);
-    printf("Num of vertex is: %d\n", n);
     
-    int lineCount = 0;
-    G = newGraph(n);
-
-    while (fgets(line, BUFF, in) != NULL) {
-        lineCount++;
-    }
-    lineCount--;
-
-    rewind(in);
-    int tempCount = 0;
-
-    while(!feof (in) && (u != 0 && v != 0)) {
-        if (tempCount > 0) {
+    int i = 0;
+    u = 1;
+    v = 1;
+    //while (fscanf(in, "%[^\n]", 10) != EOF) {
+    while ((v != 0 && v != 0)) {
+        if (i == 0) {
+            fscanf(in, "%d", &n);
+            G = newGraph(n);
+        } else {
             fscanf(in, "%d %d", &u, &v);
-            printf("u is %d and v is %d\n", u, v);
+            if (u == 0 && v == 0) {
+                break;
+            }
+            addEdge(G, u, v);
+            //printf("U is %d, V is %d\n", u, v);
         }
-        tempCount++;
-    }
-    
-    
-    /*
-    while (fgets(line, MAX_LEN, in) != NULL || fgets(line, MAX_LEN, in) != "0 0") {
-        if (lineCount > 0) {
-            //fscanf(in, "%d %d", &u, &v);
-            //fgets(x1, BUFF, in);
-            //addEdge(G, u, v);
-            //printf("%s\n", x1);
-            printf("Line is %s\n", line);
-            extract(&line);
-        }
+        i++;
     }
 
-*/
-
-    printf("Line count is %d\n", lineCount);
-
-    /*rewind(in);
-    while (u != 0 && v != 0) {
-        fscanf(in, "%d %d", &u, &v);
-        addEdge(G, u, v);
-    }
- */
     printGraph(stdout, G);
 
 
