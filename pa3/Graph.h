bc8257191e204339e6dab8f5df7ffd23ce7673af
Evin Odisho <edodisho@ucsc.edu>
2022-04-17 22:28:27 -0700

    new graph header made

---
+++ pa3/Graph.h
@@ -0,0 +1,40 @@
+/*
+ * Evin Odisho
+ * edodisho
+ * pa3
+ * Graph.h
+*/
+
+#include "List.h"
+#include <stdio.h>
+#pragma once
+
+#define INF (-999)
+#define NIL (-1)
+
+/*** Exported Type **/
+typedef struct GraphObj* Graph;
+
+/*** Constructors-Destructors ***/
+Graph newGraph(int n);
+void freeGraph(Graph* pG);
+
+/*** Access functions ***/
+int getOrder(Graph G);
+int getSize(Graph G);
+int getParent(Graph G, int u); /* Pre: 1<=u<=n=getOrder(G) */
+int getDiscover(Graph G, int u); /* Pre: 1<=u<=n=getOrder(G) */
+int getFinish(Graph G, int u); /* Pre: 1<=u<=n=getOrder(G) */
+
+// Manipulation procedures
+
+/*** Manipulation procedures ***/
+void addArc(Graph G, int u, int v);  /* Pre: 1<=u<=n, 1<=v<=n */
+void addEdge(Graph G, int u, int v);  /* Pre: 1<=u<=n, 1<=v<=n */
+void DFS(Graph G, List S); /* Pre: length(S)==getOrder(G) */
+
+
+/*** Other Functions ***/
+Graph transpose(Graph G);
+Graph copyGraph(Graph G);
+void printGraph(FILE* out, Graph G);
