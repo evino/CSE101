e187132c46a5ccb0cdfd1a73c50241132a6bc664
Evin Odisho <edodisho@ucsc.edu>
2022-05-01 17:38:39 -0700

    Test simplified

--- pa4/MatrixTest.c
+++ pa4/MatrixTest.c
@@ -1,209 +1,53 @@
-//-----------------------------------------------------------------------------
-//  MatrixTest.c 
-//  A test client for the Matrix ADT built off MatrixClient.c
-//-----------------------------------------------------------------------------
-#include<stdlib.h>
-#include<stdio.h>
-#include<stdbool.h>
-#include"Matrix.h"
+//simplified test to figure out segfault from changeEntry_getNNZ
 
-int main(){
-    int n = 5000;
-    //int n=100000;
-    Matrix A = newMatrix(n);
-    Matrix B = newMatrix(n);
-    //Matrix C, D, E, F, G, H;
-    printf("Matrices made\n");
+#include "Matrix.h"
 
-    changeEntry(A, 1,1,1); changeEntry(B, 1,1,1);
-    changeEntry(A, 1,2,2); changeEntry(B, 1,2,0);
-    changeEntry(A, 1,3,3); changeEntry(B, 1,3,1);
-    changeEntry(A, 2,1,4); changeEntry(B, 2,1,0);
-    changeEntry(A, 2,2,5); changeEntry(B, 2,2,1);
-    changeEntry(A, 2,3,6); changeEntry(B, 2,3,0);
-    changeEntry(A, 3,1,7); changeEntry(B, 3,1,1);
-    changeEntry(A, 3,2,8); changeEntry(B, 3,2,1);
-    changeEntry(A, 3,3,9); changeEntry(B, 3,3,1);
+int main(int argc, char **argv) {
+    Matrix A = newMatrix(10);
+
+    changeEntry(A, 2, 1, 2);
+    changeEntry(A, 3, 1, 5);
+    changeEntry(A, 1, 2, 2);
+    changeEntry(A, 1, 3, 5);
+    changeEntry(A, 1, 1, 4);
+    changeEntry(A, 2, 2, 2);
+    changeEntry(A, 2, 5, 0);
+    changeEntry(A, 2, 3, 0);
+    changeEntry(A, 3, 3, 5);
+    printf("A's NNZ is %d\n", NNZ(A));
+    if (NNZ(A) != 7)
+      return 1;
     
-    printf("Changed entries\n");
-    /*
 
-    printf("%d\n", NNZ(A));
-    printMatrix(stdout, A);
-    printf("\n");
+    changeEntry(A, 1, 3, 0);
 
-    printf("%d\n", NNZ(B));
-    printMatrix(stdout, B);
-    printf("\n");
+    changeEntry(A, 3, 1, 0);
 
-    C = scalarMult(1.5, A);
-    printf("%d\n", NNZ(C));
-    printMatrix(stdout, C);
-    printf("\n");
+    printf("A's NNZ is now %d\n", NNZ(A));
 
-    D = sum(A, B);
-    printf("%d\n", NNZ(D));
-    printMatrix(stdout, D);
-    printf("\n");
+    
+    changeEntry(A, 3, 3, 0);  // This line is causing a segfault
+    
+    printf("DB\n");
 
-    E = diff(A, A);
-    printf("%d\n", NNZ(E));
-    printMatrix(stdout, E);
-    printf("\n");
 
-    F = transpose(B);
-    printf("%d\n", NNZ(F));
-    printMatrix(stdout, F);
-    printf("\n");
+    if (NNZ(A) != 4)
+      return 2;
 
-    G = product(B, B);
-    printf("%d\n", NNZ(G));
-    printMatrix(stdout, G);
-    printf("\n");
+    
 
-    H = copy(A);
-    printf("%d\n", NNZ(H));
-    printMatrix(stdout, H);
-    printf("\n");
+    changeEntry(A, 7, 6, 42);
+    changeEntry(A, 10, 1, 24);
+    if (NNZ(A) != 6)
+      return 3;
+    changeEntry(A, 7, 6, 0);
+    if (NNZ(A) != 5)
+      return 4;
 
-    printf("%s\n", equals(A, H)?"true":"false" );
-    printf("%s\n", equals(A, B)?"true":"false" );
-    printf("%s\n", equals(A, A)?"true":"false" );
 
     makeZero(A);
-    printf("%d\n", NNZ(A));
-    printMatrix(stdout, A);
-    */
-
-    Matrix C;
-    C = copy(A);
-    printf("Printing Matrix A (%d NNZ's)\n", NNZ(A));
-    printMatrix(stdout, A);
-    printf("Matrix B:\n");
-    printMatrix(stdout, B);
-    printf("Matrix C:\n");
-    printMatrix(stdout, C);
-
-    if (equals(A, C) == 1) {
-        printf("A and C are equal\n");
-    } else {
-        printf("A and C are NOT equal\n");
-    }
-
-
-    Matrix T;
-    T = transpose(A);
-    printf("Transpose of A:\n");
-    printMatrix(stdout, T);
-
-    Matrix Z;
-    Z = copy(B);
-    printf("Matrix Z is\n");
-    printMatrix(stdout, Z);
-
-    printf("Calling makeZero on B\n");
-    //makeZero(B);
-    printMatrix(stdout, B);
-
-    Matrix S;
-    
-    S = scalarMult(3, A);
-    printf("Scalar S:\n");
-    printMatrix(stdout, S);
-
-    printf("Going to multiply A and B\n");
-    printf("Matrix A:\n");
-    printMatrix(stdout, A);
-    printf("Matrix C:\n");
-    printMatrix(stdout, C);
-
-    printf("Product of A & C:\n");
-    Matrix P = product(A,C);
-    printMatrix(stdout, P);
-    printf("Matrix P should have been printed\n");
-
-    
-    printf("Adding A & Z\n");
-    printf("A:\n");
-    printMatrix(stdout, A);
-    printf("Z:\n");
-    printMatrix(stdout, Z);
-    Matrix sumTest;
-    sumTest = sum(A, Z);
-    printf("Sum is:\n");
-    printMatrix(stdout, sumTest);
-
-
-    printf("Subtraction is:\n");
-    Matrix subTest;
-    subTest = diff(A, Z);
-    printf("Sub is:\n");
-    printMatrix(stdout, subTest);
-    
-    
-    printf("About to call free");
-
-    freeMatrix(&A);
-    freeMatrix(&B);
-    freeMatrix(&C);
-    freeMatrix(&T);
-    freeMatrix(&S);
-
-    printf("Matrices A & B freed\n");
-
-    /*
-    freeMatrix(&C);
-    freeMatrix(&D);
-    freeMatrix(&E);
-    freeMatrix(&F);
-    freeMatrix(&G);
-    freeMatrix(&H);
-    */
-
-    return EXIT_SUCCESS;
-    }
-
-    /*
-    Output of this program:
-    9
-    1: (1, 1.0) (2, 2.0) (3, 3.0)
-    2: (1, 4.0) (2, 5.0) (3, 6.0)
-    3: (1, 7.0) (2, 8.0) (3, 9.0)
-
-    6
-    1: (1, 1.0) (3, 1.0)
-    2: (2, 1.0)
-    3: (1, 1.0) (2, 1.0) (3, 1.0)
-
-    9
-    1: (1, 1.5) (2, 3.0) (3, 4.5)
-    2: (1, 6.0) (2, 7.5) (3, 9.0)
-    3: (1, 10.5) (2, 12.0) (3, 13.5)
-
-    9
-    1: (1, 2.0) (2, 2.0) (3, 4.0)
-    2: (1, 4.0) (2, 6.0) (3, 6.0)
-    3: (1, 8.0) (2, 9.0) (3, 10.0)
-
-    0
-
-    6
-    1: (1, 1.0) (3, 1.0)
-    2: (2, 1.0) (3, 1.0)
-    3: (1, 1.0) (3, 1.0)
-
-    7
-    1: (1, 2.0) (2, 1.0) (3, 2.0)
-    2: (2, 1.0)
-    3: (1, 2.0) (2, 2.0) (3, 2.0)
-
-    9
-    1: (1, 1.0) (2, 2.0) (3, 3.0)
-    2: (1, 4.0) (2, 5.0) (3, 6.0)
-    3: (1, 7.0) (2, 8.0) (3, 9.0)
-
-    true
-    false
-    true
-    0
-    */
+    changeEntry(A, 5, 5, 5);
+    if (NNZ(A) != 1)
+      return 5;
+    return 0;
+}
