fb9981ac75105bfb06b6e45ffb19a7ff189be7a6
Evin Odisho <edodisho@ucsc.edu>
2022-05-01 21:02:11 -0700

    Adding tests

--- pa4/MatrixTest.c
+++ pa4/MatrixTest.c
@@ -4,6 +4,7 @@
 
 int main(int argc, char **argv) {
     Matrix A = newMatrix(10);
+    Matrix B = newMatrix(10);
 
     changeEntry(A, 1, 1, -4);
     changeEntry(A, 1, 2, -2);
@@ -19,6 +20,20 @@ int main(int argc, char **argv) {
     Matrix d = diff(A, A);
 
     printf("D's NNZ now is %d\n", NNZ(d));
+
+
+    changeEntry(B, 1, 1, -4);
+    changeEntry(B, 1, 2, 0);
+    changeEntry(B, 2, 1, 0);
+    changeEntry(B, 2, 2, -2);
+    changeEntry(B, 2, 4, 2);
+    changeEntry(B, 3, 1, 2);
+    changeEntry(B, 3, 2, 2);
+    changeEntry(B, 7, 8, 5);
+
+    Matrix z = diff(A, B);
+
+    printf("Z's nnz is %d\n", NNZ(z));
 
     return 0;
   }
