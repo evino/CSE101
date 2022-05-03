cfe35056a18552d3d07adb77d556435e137e52ec
Evin Odisho <edodisho@ucsc.edu>
2022-05-02 21:12:39 -0700

    Need to test constructors

---
+++ pa5/List.cpp
@@ -0,0 +1,59 @@
+/*
+   Evin Odisho
+   edodisho
+   PA5
+   List.cpp
+*/
+
+#include "List.h"
+#include <stdexcept>
+
+// Private Constructor --------------------------------------------------------
+
+// Node Constructor
+List::Node::Node(ListElement x) {
+    data = x;
+    next = nullptr;
+    prev = nullptr;
+}
+
+// Class Constructors & Destructors -------------------------------------------
+List::List() {
+
+    frontDummy->next = backDummy;
+    backDummy->prev = frontDummy;
+    beforeCursor = nullptr;
+    afterCursor = nullptr;
+    pos_cursor = 0;
+    num_elements = 0;
+}
+
+// Copy Constructor
+List::List(const List& L) {
+    // Makes empty List
+    frontDummy->next = backDummy;
+    backDummy->prev = frontDummy;
+    beforeCursor = nullptr;
+    afterCursor = nullptr;
+    pos_cursor = 0;
+    num_elements = 0;
+
+    Node *N = L.frontDummy->next;
+    while (N != nullptr) {
+        insertAfter(N->data);
+        N = N->next;
+    }
+}
+
+// Destructor
+List::~List() {
+    Node *N = L.frontDummy->next;
+    Node *T;
+    while (length() > 0) {
+        T = N->next;
+        eraseAfter();
+        N = T;
+    }
+}
+
+
