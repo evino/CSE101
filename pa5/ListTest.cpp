cc35883b99ae46f9c74a5722f8a9be1fa55bd36b
Evin Odisho <edodisho@ucsc.edu>
2022-05-03 18:34:10 -0700

    Adding tests

--- pa5/ListTest.cpp
+++ pa5/ListTest.cpp
@@ -9,9 +9,14 @@
 
 int main(int argc, char **argv) {
     List A;
-    std::cout << "DB" << std::endl;
-    A.insertAfter(5);
     std::cout << "List A made" << std::endl;
+    std::cout << "A's length is " << A.length() << std::endl;
+    std::cout << "Inserting 5 after cursor" << std::endl;
+    A.insertAfter(5);
+    std::cout << "A's pos currently is " << A.position() << std::endl;
+    std::cout << "A's length is " << A.length() << std::endl;
+    std::cout << "A's front value is " << A.front() << std::endl;
 
-    return 0;
+    std::cout << "\nBefore return" << std::endl;
+    return EXIT_SUCCESS;
 }
