1d5af50e1baa33c5f18d99c51f0dfccacdaaeba0
Evin Odisho <edodisho@ucsc.edu>
2022-05-04 00:22:06 -0700

    Added test for setAfter

--- pa5/ListTest.cpp
+++ pa5/ListTest.cpp
@@ -10,12 +10,17 @@
 int main(int argc, char **argv) {
     List A;
     std::cout << "List A made" << std::endl;
-    std::cout << "A's length is " << A.length() << std::endl;
+    std::cout << "(EMPTY LIST) A's length is " << A.length() << std::endl;
     std::cout << "Inserting 5 after cursor" << std::endl;
     A.insertAfter(5);
-    std::cout << "A's pos currently is " << A.position() << std::endl;
-    std::cout << "A's length is " << A.length() << std::endl;
+    A.insertAfter(6);
+    A.insertAfter(7);
     std::cout << "A's front value is " << A.front() << std::endl;
+    A.moveFront();
+    A.setAfter(1);
+    std::cout << "A's pos currently is " << A.position() << std::endl;
+    std::cout << "A's length now is " << A.length() << std::endl;
+    std::cout << "A's front value now is " << A.front() << std::endl;
 
     A.eraseAfter();
     std::cout << "A's length now is " << A.length() << std::endl;
