62199618d1a68e8013867b65fbea62d3847ae9fd
Evin Odisho <edodisho@ucsc.edu>
2022-05-04 19:13:41 -0700

    making tests simple

--- pa5/ListTest.cpp
+++ pa5/ListTest.cpp
@@ -8,24 +8,42 @@
 #include "List.h"
 
 int main(int argc, char **argv) {
+    //List A;
+    //A.insertAfter(5);
+    //A.insertAfter(2);
+    //A.insertAfter(15);
+    //A.eraseAfter();
     List A;
-    std::cout << "List A made" << std::endl;
-    std::cout << "(EMPTY LIST) A's length is " << A.length() << std::endl;
-    std::cout << "Inserting 5 after cursor" << std::endl;
+    //std::cout << "List A made" << std::endl;
+    //std::cout << "(EMPTY LIST) A's length is " << A.length() << std::endl;
+    //std::cout << "Inserting 5 after cursor" << std::endl;
     A.insertAfter(5);
     A.insertAfter(6);
     A.insertAfter(7);
-    std::cout << "A's front value is " << A.front() << std::endl;
+    //std::cout << "A's front value is " << A.front() << std::endl;
     A.moveFront();
-    std::cout << "Front peek next is " << A.peekNext() << std::endl;
+    std::cout << "Pos is " << A.position() << std::endl;
+    //std::cout << "Front peek next is " << A.peekNext() << std::endl;
     A.setAfter(1);
-    std::cout << "Calling moveNext on front " << A.moveNext() << std::endl;
-    std::cout << "A's pos currently is " << A.position() << std::endl;
-    std::cout << "A's length now is " << A.length() << std::endl;
-    std::cout << "A's front value now is " << A.front() << std::endl;
+    std::cout << "A: " << A.to_string() << std::endl;
+    std::cout << "Pos is " << A.position() << std::endl;
+    //std::cout << "Front peek next is " << A.peekNext() << std::endl;
 
-    A.eraseAfter();
-    std::cout << "A's length now is " << A.length() << std::endl;
-    std::cout << "\nBefore return" << std::endl;
+    //std::cout << "A now is: " << A.to_string() << std::endl;
+    std::cout << "6 found at position " << A.findNext(6) << std::endl;
+    std::cout << "Pos is " << A.position() << std::endl;
+    //std::cout << "A's pos is " << A.position() << std::endl;
+    //std::cout << "A len: " << A.length() << std::endl;
+    std::cout << "Calling moveNext on front. returning: " << A.moveNext() << std::endl;
+    //std::cout << "A's pos currently is " << A.position() << std::endl;
+    //std::cout << "A's length now is " << A.length() << std::endl;
+    //std::cout << "A's front value now is " << A.front() << std::endl;
+
+    //std::cout << "DB1" << std::endl;
+    ////A.eraseAfter();
+    //std::cout << "DB2" << std::endl;
+    ////A.eraseAfter();
+    //std::cout << "A's length now is " << A.length() << std::endl;
+    //std::cout << "\nBefore return" << std::endl;
     return EXIT_SUCCESS;
 }
