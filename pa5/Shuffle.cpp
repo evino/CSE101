ed6162cfffffda08e868604a085685d59290e682
Evin Odisho <edodisho@ucsc.edu>
2022-05-06 22:35:03 -0700

    Need to fix conditions. Last two elements are not being inserted

--- pa5/Shuffle.cpp
+++ pa5/Shuffle.cpp
@@ -16,19 +16,36 @@
 void Shuffle(List& D) {
     List A;
     List B;
-    int A_elems = (length() / 2);
-    int B_elems = (length() - A_elems);
+    int A_elems = (D.length() / 2);
+    int B_elems = (D.length() - A_elems);
 
     for (int i = 1; i <= A_elems; i++) {
         A.insertBefore(i);
     }
 
-    for (int j = B_elems; j <= length(); j++) {
+    for (int j = B_elems; j <= D.length(); j++) {
         B.insertBefore(j);
+    }
+
+    D.clear();
+    A.moveFront();
+    B.moveFront();
+
+    while (B.peekNext() != B.back()) {
+        D.insertBefore(B.peekNext());
+        B.moveNext();
+        // Can use if statement instead since only going through once
+        if (A.peekNext() != A.back()) {
+            //D.insertBefore(B.peekNext());
+            D.insertBefore(A.peekNext());
+            A.moveNext();
+            //B.moveNext();
+        }
     }
 
     std::cout << "List A: " << A.to_string() << std::endl;
     std::cout << "List B: " << B.to_string() << std::endl;
+    std::cout << "D: " << D.to_string() << std::endl;
 
     return;
 }
@@ -38,9 +55,9 @@ int main (int argc, char **argv) {
     List D;
     List S;
     for (int i = 1; i <= 7; i++) {
-        A.insertBefore(i);
+        D.insertBefore(i);
     }
-    shuffle(A);
+    Shuffle(D);
 
 
     return 0;
