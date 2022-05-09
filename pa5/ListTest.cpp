e3a008d4e077748a43c188a80b06462c0f55c80f
Evin Odisho <edodisho@ucsc.edu>
2022-05-08 22:18:16 -0700

    Added some final tests

--- pa5/ListTest.cpp
+++ pa5/ListTest.cpp
@@ -5,19 +5,20 @@
     ListTest.cpp
 */
 
+
+// My test harness for PA5's List ADT in C++. Harness shares a few lines
+// with the ListClient.cpp file, but has more simplified tests that were used
+// throughout ADT development and testing. As each new function was implemented,
+// the function was called within this file to test it.
+
 #include "List.h"
 
 int main(int argc, char **argv) {
-    //List A;
-    //A.insertAfter(5);
-    //A.insertAfter(2);
-    //A.insertAfter(15);
-    //A.eraseAfter();
     List A;
     List B;
-    //std::cout << "List A made" << std::endl;
+    std::cout << "List A made" << std::endl;
     //std::cout << "(EMPTY LIST) A's length is " << A.length() << std::endl;
-    //std::cout << "Inserting 5 after cursor" << std::endl;
+    std::cout << "Inserting 5 after cursor" << std::endl;
     A.insertAfter(5);
     A.insertAfter(6);
     A.insertAfter(7);
@@ -32,24 +33,21 @@ int main(int argc, char **argv) {
     } else {
         std::cout << "False!" << std::endl;
     }
-    //std::cout << "A's front value is " << A.front() << std::endl;
+    std::cout << "A's front value is " << A.front() << std::endl;
     A.moveFront();
     std::cout << "Pos is " << A.position() << std::endl;
-    //std::cout << "Front peek next is " << A.peekNext() << std::endl;
+    std::cout << "Front peek next is " << A.peekNext() << std::endl;
     A.setAfter(1);
     std::cout << "A: " << A.to_string() << std::endl;
     std::cout << "Pos is " << A.position() << std::endl;
-    //std::cout << "Front peek next is " << A.peekNext() << std::endl;
 
-    //std::cout << "A now is: " << A.to_string() << std::endl;
+    std::cout << "A now is: " << A.to_string() << std::endl;
     std::cout << "6 found at position " << A.findNext(6) << std::endl;
     std::cout << "Pos is " << A.position() << std::endl;
-    //std::cout << "A's pos is " << A.position() << std::endl;
-    //std::cout << "A len: " << A.length() << std::endl;
+    std::cout << "A len: " << A.length() << std::endl;
     std::cout << "Calling moveNext on front. returning: " << A.moveNext() << std::endl;
     std::cout << "A's pos currently is " << A.position() << std::endl;
-    //std::cout << "A's length now is " << A.length() << std::endl;
-    //std::cout << "A's front value now is " << A.front() << std::endl;
+    std::cout << "A's front value now is " << A.front() << std::endl;
 
     //std::cout << "DB1" << std::endl;
     std::cout << "A: " << A.to_string() << std::endl;
@@ -78,7 +76,7 @@ int main(int argc, char **argv) {
 
 
  
-    // Replicating Cleanup_size test 1:
+    // Replicating Cleanup_size test 1 (DEBUG):
     std::cout << "\n\nCleanup_size test 1:" << std::endl;
     A.clear();
     A.insertAfter(1);
@@ -93,8 +91,52 @@ int main(int argc, char **argv) {
     A.cleanup();
     std::cout << "A now after cleanup now is: " << A.to_string() << std::endl;
 
+    List C = B;
+    C.moveBack();
+    std::cout << "List C: " << C << std::endl;
 
+    if (C == B) {
+        std::cout << "Lists B & C are equal" << std::endl;
+    } else {
+        std::cout << "Lists B & C are NOT equal" << std::endl;
+    }
 
-    std::cout << "\nBefore return" << std::endl;
+    std::cout << "Inserting (";
+    for (int to_insert = 15; to_insert <= 20; to_insert++) {
+        C.insertAfter(to_insert);
+        std::cout << to_insert << " ";
+    }
+    
+    std::cout << ") Into list C" << std::endl;
+    std::cout << "List C now is: " << C << std::endl;
+    std::cout << "B is: " << B.to_string() << std::endl;
+
+    if (!(B == C)) { // Should not be equal
+        std::cout << "There for B & C are NOT equal now." << std::endl;
+    } else {
+        std::cout << "The two are equal" << std::endl;
+    }
+
+    C.clear();
+    std::cout << "Calling clear() on List C" << std::endl;
+    std::cout << "C now is " << C << std::endl;
+
+    B.clear();
+    A.clear();
+    std::cout << "Clearing A and B now" << std::endl;
+    std::cout << "List B: " << B << std::endl;
+    std::cout << "List A: " << A << std::endl;
+
+    if ((A == B) && (A == C)) {
+        std::cout << "All Lists now are clear and empty, and equal." << std::endl;
+    } else {
+        std::cout << "All the lists are not equal" << std::endl;
+    }
+
+    std::cout << "A's length is: " << A.length() << std::endl;
+    std::cout << "B's length is: " << B.length() << std::endl;
+    std::cout << "C's length is: " << C.length() << std::endl;
+
+    std::cout << "\n\n END OF TEST REACHED" << std::endl;
     return EXIT_SUCCESS;
 }
