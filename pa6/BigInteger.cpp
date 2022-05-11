3ae29f2ea1f108da95dc09c5ed61c2f2c97f40dc
Evin Odisho <edodisho@ucsc.edu>
2022-05-11 00:13:33 -0700

    Trying to catch exception

--- pa6/BigInteger.cpp
+++ pa6/BigInteger.cpp
@@ -10,16 +10,25 @@ BigInteger::BigInteger() {
 }
 
 BigInteger::BigInteger(std::string s) {
+    try {
+        std::stol(s[0], nullptr, 10);
+    }
+    catch (std::invalid_argument) {
+        std::cout << "caught" << std::endl;
+    }
+    std::cout << "After isDig" << std::endl;
     if (s.empty()) {
         throw std::invalid_argument("BigInteger: Constructor: empty string");
     }
+
     if (s[0] != '-' || s[0] != '+' || !isdigit(s[0])) {
         throw std::invalid_argument("BigInteger: Constructor: non-numeric string");
     }
     for (int i = 0; i <= s.length(); i++) {
+        
         if (!isdigit(s[i])) {
             throw std::invalid_argument("BigInteger: Constructor: non-numeric string");
         }
     }
 
-
+}
