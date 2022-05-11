9cc642999ff9f39e90bcca7adbb9aa34d9307b3a
Evin Odisho <edodisho@ucsc.edu>
2022-05-11 14:15:33 -0700

    Still testing string constructor

--- pa6/BigIntegerClient.cpp
+++ pa6/BigIntegerClient.cpp
@@ -1,7 +1,7 @@
 #include "BigInteger.h"
 int main(int argc, char **argv) {
     BigInteger A;
-    std::string s = "h123";
+    std::string s = "12345678910111213141516171819202122";
     A = BigInteger(s);
     return 0;
 }
