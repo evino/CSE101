39fd6a92d8baee5f84d604c242622023fb2bad94
Evin Odisho <edodisho@ucsc.edu>
2022-05-11 23:25:55 -0700

    Added print for debug/more tests

--- pa6/BigIntegerClient.cpp
+++ pa6/BigIntegerClient.cpp
@@ -3,5 +3,6 @@ int main(int argc, char **argv) {
     BigInteger A;
     std::string s = "-12345678910111213141516171819202122";
     A = BigInteger(s);
+    std::cout << A.to_string() << std::endl;
     return 0;
 }
