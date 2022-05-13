6317a1e78deaa2c113c16c05f1c48f1a4a384ff4
Evin Odisho <edodisho@ucsc.edu>
2022-05-12 18:36:08 -0700

    I think the to_string() func works now

--- pa6/BigIntegerClient.cpp
+++ pa6/BigIntegerClient.cpp
@@ -2,12 +2,16 @@
 int main(int argc, char **argv) {
     BigInteger A;
     BigInteger B;
-    std::string s = "12345678910111213141516171819202123";
-    std::string s2 = "12345678910111213141516171819202121";
+    //std::string s = "12345678910111213141516171819202123";
+    //std::string s2 = "00012345678910111213141516171819202121";
     //std::string s2 = "9876545439000000000000000100000000000006543654365346534";
     //std::string s2 = "0";
+    
+    std::string s = "0050";
+    std::string s2 = "00500";
     A = BigInteger(s);
     B = BigInteger(s2);
+    //A.makeZero();
     std::cout << "A: " << A.to_string() << std::endl;
     std::cout << "B: " << B.to_string() << std::endl;
     std::cout << A.compare(B) << std::endl;
