29a8b07f05ebbc7b115073f7bcad4fb70ecf0209
Evin Odisho <edodisho@ucsc.edu>
2022-05-13 14:17:04 -0700

    Swapping out some test

--- pa6/BigIntegerClient.cpp
+++ pa6/BigIntegerClient.cpp
@@ -2,13 +2,11 @@
 int main(int argc, char **argv) {
     BigInteger A;
     BigInteger B;
-    //std::string s = "12345678910111213141516171819202123";
+    std::string s = "12345678910111213141516171819202123";
     //std::string s2 = "00012345678910111213141516171819202121";
-    //std::string s2 = "9876545439000000000000000100000000000006543654365346534";
+    std::string s2 = "9876545439000000000000000100000000000006543654365346534";
     //std::string s2 = "0";
     
-    std::string s = "10";
-    std::string s2 = "9";
     A = BigInteger(s);
     B = BigInteger(s2);
     //A.makeZero();
