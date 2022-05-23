9965ee49a43ee3ebc489a90ba0b1ce61fc703285
Evin Odisho <edodisho@ucsc.edu>
2022-05-22 23:25:49 -0700

    Added tests for pre_string()

--- pa7/DictionaryTest.cpp
+++ pa7/DictionaryTest.cpp
@@ -4,6 +4,15 @@ using namespace std;
 int main(int argc, char **argv) {
     cout << "First call of constructor" << endl;
     Dictionary A;
+    string S[] = {"first", "second"};
+    for (int i = 0; i < 8; i++) {
+        cout << "I = " << i << endl;
+        A.setValue(S[i], i+1);
+    }
+
+    cout << "Calling pre_string() on A: " << A.pre_string() << endl;
+    cout << "A's size: " << A.size() << endl;
+
     Dictionary B = Dictionary(A);
     cout << "Copy constructor called" << endl;
     cout << "B's size: " << B.size() << endl;
